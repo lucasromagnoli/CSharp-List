92762e9a9c1d83623f78a9cb5b54ac3184fa54af
Lucas Romagnoli <lucasr.romagnoli@gmail.com>
2018-03-17 21:14:14 -0300

    Acrescentando descrição e testando o push pelo visual studio.

--- CSharp-List/Program.cs
+++ CSharp-List/Program.cs
@@ -4,12 +4,19 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+/* Exemplos em C#
+ *
+ * Como criar uma lista, ordenar a mesma e verificar se determinado item da lista possui certo atributo ou não.
+ * 
+ */
+
 namespace CSharp_List
 {
     class Program
     {
         static void Main(string[] args)
         {
+
         }
     }
 }
